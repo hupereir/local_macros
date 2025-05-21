f2e7a627c2c41a49450fa9b1396e8e26cbc055bf
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2025-05-21 13:35:18 -0400

    one more macro

---
+++ ReadEvaluationFile.C
@@ -0,0 +1,119 @@
+
+R__LOAD_LIBRARY(libg4eval_hp_lite.so)
+
+
+namespace
+{
+
+  // global variables
+  TrackingEvaluator_hp::Container* container = nullptr;
+  TTree* tree = nullptr;
+
+  // square
+  double square(double x)
+  { return x*x; }
+
+  // radius
+  double get_r(double x, double y)
+  { return std::sqrt(square(x)+square(y)); }
+}
+
+//____________________________________________________________________________
+void DrawEvent( int i )
+{
+
+  // load first event
+  tree->GetEntry(i);
+
+  // print list of tracks
+  std::cout << "tracks: " << container->tracks().size() << std::endl;
+
+  // create canvas
+  TCanvas* cv = new TCanvas( "cv", "", 900, 450 );
+  cv->Divide( 2, 1 );
+  cv->SetTopMargin(0.05);
+
+  {
+    // x,y view
+    cv->cd(1);
+
+    // create empty histogram to show axis
+    TH2* h0 = new TH2I( "h0", "", 200, -100, 100, 200, -100, 100 );
+    h0->GetXaxis()->SetTitle( "x (cm)" );
+    h0->GetYaxis()->SetTitle( "y (cm)" );
+    h0->SetStats(0);
+    h0->Draw();
+
+    // create generic marker to show clusters
+    TMarker* marker( new TMarker() );
+    marker->SetMarkerStyle(20);
+    marker->SetMarkerSize(0.6);
+    marker->SetMarkerColor(2);
+
+    // loop over container tracks
+    for( const TrackingEvaluator_hp::TrackStruct& track:container->tracks() )
+    {
+
+      std::cout << "PID: " << track._pid << std::endl;
+
+      // loop over track clusters
+      for( const TrackingEvaluator_hp::ClusterStruct& cluster:track._clusters )
+      {
+        marker->DrawMarker( cluster._x, cluster._y );
+      }
+
+    }
+
+  }
+
+  {
+    // r,z view
+    cv->cd(2);
+
+    // create empty histogram to show axis
+    TH2* h0 = new TH2I( "h1", "", 200, -100, 100, 200, -100, 100 );
+    h0->GetXaxis()->SetTitle( "z (cm)" );
+    h0->GetYaxis()->SetTitle( "r (cm)" );
+    h0->SetStats(0);
+    h0->Draw();
+
+    // create generic marker to show clusters
+    TMarker* marker( new TMarker() );
+    marker->SetMarkerStyle(20);
+    marker->SetMarkerSize(0.6);
+    marker->SetMarkerColor(2);
+
+    // loop over container tracks
+    for( const TrackingEvaluator_hp::TrackStruct& track:container->tracks() )
+    {
+
+      std::cout << "PID: " << track._pid << std::endl;
+
+      // loop over track clusters
+      for( const TrackingEvaluator_hp::ClusterStruct& cluster:track._clusters )
+      {
+        double r = get_r( cluster._x, cluster._y );
+        if( cluster._y<0) r*=-1;
+        marker->DrawMarker( cluster._z, r );
+      }
+
+    }
+  }
+
+}
+
+//____________________________________________________________________________
+void ReadEvaluationFile()
+{
+  // const TString inputFile = "DST/CONDOR_upsilon_pythia8/dst_eval_upsilon_pythia8_0.root";
+  const TString inputFile = "DST/CONDOR_jpsi_pythia8/dst_eval_jpsi_pythia8_0.root";
+
+  auto tfile = TFile::Open( inputFile );
+
+  // load tree and assign branch
+  tree = static_cast<TTree*>( tfile->Get("T") );
+  tree->SetBranchAddress( "DST#EVAL#TrackingEvaluator_hp::Container", &container );
+
+  DrawEvent(0);
+
+}
